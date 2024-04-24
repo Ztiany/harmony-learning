b2a60b0ea5cdb7651dea44f7f89a852aefe22490
ztiany <ztiany3@gmail.com>
2024-04-24 23:17:07 +0800

    add official demos

---
+++ stage-mode/step04_state_management/entry/src/main/ets/entryability/EntryAbility.ts
@@ -0,0 +1,20 @@
+import hilog from '@ohos.hilog';
+import UIAbility from '@ohos.app.ability.UIAbility';
+import Window from '@ohos.window';
+
+export default class EntryAbility extends UIAbility {
+    onWindowStageCreate(windowStage: Window.WindowStage) {
+        // Main window is created, set main page for this ability
+        hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+
+        windowStage.loadContent('pages/MainPage', (err, data) => {
+            if (err.code) {
+                hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.ERROR);
+                hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+                return;
+            }
+            hilog.isLoggable(0x0000, 'testTag', hilog.LogLevel.INFO);
+            hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+        });
+    }
+}
